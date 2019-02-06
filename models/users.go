69a219f531b0994d279bc164efb9569c5c0f9de5
Sree Krishna Raja K <sreekrishnarajak@gmail.com>
2019-02-06 16:32:32 +0530

    Bugfix: Modals User
    * Removed unused variables Description & Cover Image in struct.

--- models/users.go
+++ models/users.go
@@ -5,10 +5,8 @@ import "gopkg.in/mgo.v2/bson"
 // Represents a user, we uses bson keyword to tell the mgo driver how to name
 // the properties in mongodb document
 type User struct {
-	ID          bson.ObjectId `bson:"_id" json:"id"`
-	Name        string        `bson:"name" json:"name"`
-	CoverImage  string        `bson:"cover_image" json:"cover_image"`
-	Description string        `bson:"description" json:"description"`
-	Age         int           `bson:"age" json:"age"`
-	Gender      string        `bson:"gender" json:"gender"`
+	ID     bson.ObjectId `bson:"_id" json:"id"`
+	Name   string        `bson:"name" json:"name"`
+	Age    int           `bson:"age" json:"age"`
+	Gender string        `bson:"gender" json:"gender"`
 }
